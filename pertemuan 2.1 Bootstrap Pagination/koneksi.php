682aa47c2b1f665d4d9022fa769eccdc877003e1
fantri <ifan.ifzn17@gmail.com>
2026-02-20 22:17:03 +0700

    Tugas Selesai

---
+++ pertemuan 2.1 Bootstrap Pagination/koneksi.php
@@ -0,0 +1,12 @@
+<?php
+    $koneksi = mysqli_connect("localhost","root","","db_mahasiswa")
+?>
+
+<!-- CREATE TABLE mahasiswa (
+    id int(100) AUTO_INCREMENT PRIMARY KEY,
+    nama varchar(100),
+    nim varchar(25),
+    prodi varchar(100),
+    email varchar(100) UNIQUE,
+    jk varchar(50)
+); -->
---
+++ pertemuan 3.1 Javascript/script.js
@@ -0,0 +1,77 @@
+var myArray = []; // Membuat array kosong
+var favoriteFruits = [ "apple", "orange", "strawberry" ]; // Array berisi string buah
+var carsInParkingLot = [ "Toyota", "Ferrari", "Lexus" ]; // Array berisi merk mobil
+var employees = [ "Billy" , "Bob", "Joe" ]; // Array berisi nama karyawan
+var primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31 ]; // Array berisi angka
+var randomVariables = [ 2, "any type works", undefined, null, true, 2.51 ]; // variabel acak
+myArray = [ "zero", "one", "two" ]; // Mengisi array kosong tadi dengan 3 elemen
+
+console.log("favoriteFruits :", favoriteFruits[2]); // Menampilkan kotak pesan berisi "buah"
+console.log("carsInParkingLot :", carsInParkingLot[2]); // // Menampilkan kotak pesan berisi "mobil"
+console.log("employees :", employees[2]); // Menampilkan kotak pesan berisi "nama"
+console.log("primeNumbers :", primeNumbers[2]); // Menampilkan kotak pesan berisi "Prime Number"
+console.log("randomVariables :", randomVariables[4]); // Menampilkan kotak pesan secara acak
+
+
+elementNumber = 0; // variabel mementukan nilai array 0
+console.log ("Dengan elementNumber =", employees[ elementNumber]);
+ // Menampilkan employees dengan array elementNumber
+myObject = {}; // objek kosong
+
+john = { 
+    firstname: "John", lastname: "Doe", fullname: "John Doe" 
+}; // membuat var john nama depan, belakang dan full nama
+
+billy = {
+    firstname: "Billy", lastname: undefined, fullname: "Billy"
+}; // membuat var billy nama depan, belakang dan full nama
+
+console.log (john.firstname,"fullname =",john.fullname); 
+// menampilkan var john dengan firtname = John  dan fullname = John Doe
+
+console.log (billy.firstname,"lastname =",billy.lastname); 
+// menampilkan var billy dengan firstname = Billy  dan lastname = undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // ====== Type variabel ========
+// var myInteger = 12; // variabel bertype Integer 
+// var myLong = 9310141419482; // variabel bertype Long 
+// var myFloat = 5.5; // variabel bertype Float 
+// var myDouble = 9310141419482.22; // variabel bertype Double 
+// var myBoolean = true; // variabel bertype Boolean True
+// var myBoolean2 = false; // variabel bertype Boolean False 
+// var myNotANumber = NaN; // Menyimpan nilai NaN (Not a Number).
+// var NaN_Example = 0 / 0; // Menyimpan NaN, karena: 0 dibagi 0 = tidak terdefinisi
+// var notDefined; // variabel yang dibuat tapi belum dikasih nilai.
+// var myNull = null; // myNull variabel yang sengaja dikosongkan
+
+// // Console Log per Variabel
+// console.log("myInteger:", myInteger); // menampilkan myInteger di console
+// console.log("myLong:", myLong); // menampilkan myLong di console
+// console.log("myFloat:", myFloat); // menampilkan myFloat di console
+// console.log("myDouble:", myDouble); // menampilkan myDouble di console
+// console.log("myBoolean:", myBoolean); // menampilkan myBoolean di console
+// console.log("myBoolean2:", myBoolean2); // menampilkan myBoolean2 di console
+// console.log("myNotANumber:", myNotANumber); // menampilkan myNotANumber di console
+// console.log("NaN_Example:", NaN_Example); // menampilkan NaN_Example di console
+// console.log("notDefined:", notDefined); // menampilkan notDefined di console
+// console.log("myNull:", myNull); // menampilkan myNull di console
